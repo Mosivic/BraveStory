afec983c8110edae811a9d9e0d4470869bcac0cc
Furo <mosiv@foxmail.com>
2024-12-11 23:33:50 +0800

    fixbug: remove repeat tasks add code line

--- src/addons/Miros/Core/Executor/FSM/FSM.cs
+++ src/addons/Miros/Core/Executor/FSM/FSM.cs
@@ -43,8 +43,7 @@ public class FSM : ExecutorBase<TaskBase>, IExecutor
             _layers[fsmArgs.Layer] = new FSMExecutor(fsmArgs.Layer, _transitionContainer, _tasks);
             _layers[fsmArgs.Layer].SetDefaultTask(stateTask); //将第一个任务设置为默认任务
         }
-
-        _tasks.Add(stateTask.Tag, stateTask);
+        
         _transitionContainer.AddTransitions(stateTask, fsmArgs.Transitions);
         _transitionContainer.AddAnyTransition(fsmArgs.AnyTransition);
     }
